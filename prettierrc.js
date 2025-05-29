9d8623625a2729c69fdcc5eb8f97301a9e624358
Drew Lee <drew@spoonlabs.com>
2025-05-29 20:06:42 +0900

    feat: mock data

---
+++ prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  arrowParen: 'singleline',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
